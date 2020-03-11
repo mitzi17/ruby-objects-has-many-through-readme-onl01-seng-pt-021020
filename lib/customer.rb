a6274fffd84d004e8e3a5d6b361bee457de27942
Mitzi Estrada <m_estrada17@live.com>
2020-03-11 06:54:28 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,41 @@
-class Customer
+class Customer 
+  attr_accessor :waiter
+  
+  @@all = [ ]
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+  end
+end
+
+  def waiters
+    meals.map do |meal|
+    meal.waiter
+    end
+  end
+  
+  
+  
+    
+  
+  
+  
+  
+  
   
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
-class Meal
+class Meal 
+  attr_accessor :customer, :waiter, :total, :tip
+  
+  @@all = [ ]
+  
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 
 end
