0e4b9227406d5131c36be5f70852ac8e7373fd85
Mitzi Estrada <m_estrada17@live.com>
2020-03-11 06:32:35 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,16 @@
 class Waiter
   
+  @@all = [ ]
+  
   def initialize(name, years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
 
 end
