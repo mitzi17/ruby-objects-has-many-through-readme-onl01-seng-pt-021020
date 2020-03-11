ceca0791f0444e604bb505dd45f5836c005777c8
Mitzi Estrada <m_estrada17@live.com>
2020-03-11 06:30:56 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,9 +1,18 @@
 class Customer
   
+  @@all = [ ]
+  
   def initialize(name, age)
     @name = name
     @age = age
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
+  
   
   
   
